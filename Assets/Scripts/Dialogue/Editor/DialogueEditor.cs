2648db660e6f2793cbcbed056597fa4e870ea493
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2023-04-29 11:37:19 +0100

    Changed type check in OnOpenAsset

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -34,7 +34,9 @@ namespace RPG.Dialogue.Editor
         [OnOpenAsset(1)]
         public static bool OnOpenAsset(int instanceID, int line)
         {
-            if(EditorUtility.InstanceIDToObject(instanceID) is Dialogue)
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+
+            if(dialogue != null)
             {
                 ShowEditorWindow();
                 return true;
