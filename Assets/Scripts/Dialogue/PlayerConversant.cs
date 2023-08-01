170f00af22e56a6d3520f8f1561d29326e64cabd
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2023-08-01 20:30:57 +0100

    Created empty script for PlayerConversant

---
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] Dialogue currentDialogue; // In the future this will be changed/set programmatically
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
