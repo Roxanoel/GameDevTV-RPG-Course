aee2646a29e0d813da555a0b2107c2e7e4208429
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2023-08-02 10:11:42 +0100

    Functionality for Next()

--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace RPG.Dialogue
 {
     public class PlayerConversant : MonoBehaviour
     {
         [SerializeField] Dialogue currentDialogue; // In the future this will be changed/set programmatically
+        DialogueNode currentNode = null;
+
+        private void Awake()
+        {
+            // Initialise currentNode as root node of current dialogue
+            currentNode = currentDialogue.GetRootNode();
+        }
 
         public string GetText()
         {
             // safeguard 
-            if (currentDialogue == null)
+            if (currentNode == null)
             {
                 return "";
             }
-            return currentDialogue.GetRootNode().GetText(); // Currently hardcoded but eventually could make the function more flexible to return text of desired node
+            return currentNode.GetText(); 
         }
 
         // Called when we want to progress the dialogue 
         public void Next()
         {
-
+            DialogueNode[] children = currentDialogue.GetAllChildren(currentNode).ToArray();
+            // For now, always using the first child
+            currentNode = children[0];
         }
 
         // Tells you if you have reached a leaf node of the dialogue tree
