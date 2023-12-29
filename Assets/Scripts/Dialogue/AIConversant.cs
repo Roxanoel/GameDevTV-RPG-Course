e82f988a779c081146a07b3834c3c87058826e75
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2023-12-29 14:06:44 -0800

    boilerplate for AIConversant

---
+++ Assets/Scripts/Dialogue/AIConversant.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Control;
+
+public class AIConversant : MonoBehaviour, IRaycastable
+{
+    public CursorType GetCursorType()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool HandleRaycast(PlayerController callingController)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
