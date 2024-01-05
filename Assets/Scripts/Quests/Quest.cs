f58bf2e74926e2afa2847ba0572fcda94f4e0891
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-05 15:32:49 -0800

    Boilerplate for Quest ScriptableObject

---
+++ Assets/Scripts/Quests/Quest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Quests
+{
+    [CreateAssetMenu(fileName = "Quest", menuName ="Quest", order = 0)]
+
+    public class Quest : ScriptableObject
+    {
+
+        
+    }
+}
