a5a1ea35169c1b905f09da5e0eb379071a69947c
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-05 16:10:32 -0800

    Hooked QuestListUI and QuestItemUI to prefabs

--- Assets/Scripts/UI/Quests/QuestItemUI.cs
+++ Assets/Scripts/UI/Quests/QuestItemUI.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using RPG.Quests;
 using UnityEngine;
 
 public class QuestItemUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TextMeshProUGUI title;
+    [SerializeField] TextMeshProUGUI progress;
+    
+    public void Setup(Quest quest)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/UI/Quests/QuestListUI.cs
+++ Assets/Scripts/UI/Quests/QuestListUI.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Quests;
 using UnityEngine;
 
 public class QuestListUI : MonoBehaviour
 {
+    [SerializeField] Quest[] tempQuests;
+    [SerializeField] QuestItemUI questPrefab;
+    
     // Start is called before the first frame update
     void Start()
     {
