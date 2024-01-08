3abb6d43528ac9bb4d9b8be22dbf7442ef0cfea6
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-08 13:57:53 -0800

    Added references to Quest UI Tooltip elements

--- Assets/Scripts/UI/Quests/QuestTooltipUI.cs
+++ Assets/Scripts/UI/Quests/QuestTooltipUI.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Quests;
 using UnityEngine;
+using TMPro;
 
 namespace RPG.UI.Quests
 {
     public class QuestTooltipUI : MonoBehaviour
     {
+        [SerializeField] TextMeshProUGUI title;
+        [SerializeField] Transform objectivesContainer;
+        [SerializeField] GameObject objectivePrefab;
+        
         public void Setup(Quest quest)
         {
             Debug.Log(quest);
