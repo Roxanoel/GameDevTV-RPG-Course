866d0b39ff81a726e00d2c3850999fa1b43a83d5
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-08 13:44:42 -0800

    Wrote getter for quest in QuestItemUI

--- Assets/Scripts/UI/Quests/QuestItemUI.cs
+++ Assets/Scripts/UI/Quests/QuestItemUI.cs
@@ -8,10 +8,18 @@ public class QuestItemUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI title;
     [SerializeField] TextMeshProUGUI progress;
+
+    Quest currentQuest;
     
     public void Setup(Quest quest)
     {
+        currentQuest = quest;
         title.text = quest.GetTitle();
         progress.text = $"{quest.GetObjectivesCount()}/{quest.GetObjectivesCount()}"; // FOR NOW
+    }
+
+    public Quest GetQuest()
+    {
+        return currentQuest;
     }
 }
