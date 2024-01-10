8839924ab1548ce17701728c8451157722c68c10
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-10 11:36:41 -0800

    Added to namespace RPG.Quests

--- Assets/Scripts/Quests/QuestStatus.cs
+++ Assets/Scripts/Quests/QuestStatus.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestStatus : MonoBehaviour
+namespace RPG.Quests
 {
-    // Start is called before the first frame update
-    void Start()
+    public class QuestStatus : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
