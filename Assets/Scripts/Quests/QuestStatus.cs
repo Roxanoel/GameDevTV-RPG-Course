4d4993cbebf5b9232004b6399b8e7f3fe1c00578
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-10 11:42:12 -0800

    Added fields to QuestStatus class

--- Assets/Scripts/Quests/QuestStatus.cs
+++ Assets/Scripts/Quests/QuestStatus.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 namespace RPG.Quests
 {
-    public class QuestStatus : MonoBehaviour
+    public class QuestStatus
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        [SerializeField] Quest quest;
+        [SerializeField] List<string> completedObjectives;
     }
 }
