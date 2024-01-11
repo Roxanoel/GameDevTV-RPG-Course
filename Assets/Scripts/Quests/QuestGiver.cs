95af7932a867eae89aa6cdb2d24ac990fcf2a15e
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-10 16:10:39 -0800

    updated example quest to use with quest giver

--- Assets/Scripts/Quests/QuestGiver.cs
+++ Assets/Scripts/Quests/QuestGiver.cs
@@ -6,17 +6,7 @@ namespace RPG.Quests
 {
     public class QuestGiver : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        [SerializeField] Quest quest;
     }
 
 }
