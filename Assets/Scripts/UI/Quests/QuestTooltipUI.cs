3049d73346f9c6a69109eaf10871988d95a4f413
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-12 13:53:08 -0800

    tooltip shows rewards

--- Assets/Scripts/UI/Quests/QuestTooltipUI.cs
+++ Assets/Scripts/UI/Quests/QuestTooltipUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using RPG.Quests;
 using UnityEngine;
 using TMPro;
@@ -11,17 +12,19 @@ namespace RPG.UI.Quests
         [SerializeField] TextMeshProUGUI title;
         [SerializeField] Transform objectivesContainer;
         [SerializeField] GameObject objectivePrefab;
+        [SerializeField] TextMeshProUGUI rewardsText;
         
         public void Setup(QuestStatus status)
         {
             Quest quest = status.GetQuest();
+            // Title
             title.text = quest.GetTitle();
             // Before populating the list of objectives make sure it's empty
             foreach (Transform item in objectivesContainer)
             {
                 Destroy(item.gameObject);
             }
-
+            // Objectives 
             foreach (Quest.Objective objective in quest.GetObjectives())
             {
                 GameObject objectiveInstance = Instantiate(objectivePrefab, objectivesContainer) as GameObject;
@@ -30,6 +33,26 @@ namespace RPG.UI.Quests
                 objectiveUI.SetTitleText(objective.description);
                 objectiveUI.SetCompletionStatus(status.IsObjectiveComplete(objective.reference));
             }
+            // Rewards
+            rewardsText.text = GetRewardsText(quest);
+        }
+
+        private string GetRewardsText(Quest quest)
+        {
+            string rewards = "";
+            foreach (Quest.Reward reward in quest.GetRewards())
+            {
+                if (rewards != "")
+                {
+                    rewards += ", ";
+                }
+                if (reward.number > 1)
+                {
+                    rewards += $"{reward.number}x ";
+                }
+                rewards += reward.item.name;
+            }
+            return rewards;
         }
     }
 }
