e86b9a6304dc03b0cf8195de4eadd5039c2d1abe
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-15 10:54:29 -0800

    QuestCompletion class with fields

---
+++ Assets/Scripts/Quests/QuestCompletion.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Quests
+{
+    public class QuestCompletion : MonoBehaviour
+    {
+        [SerializeField] Quest quest;
+        [SerializeField] string objectiveRef;
+    }
+}
