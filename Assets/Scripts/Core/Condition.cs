8d66604a148ede6ec3c5547907fb296951120cc2
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2024-01-16 10:44:04 -0800

    Created Condition class

---
+++ Assets/Scripts/Core/Condition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    [System.Serializable]
+    public class Condition
+    {
+        [SerializeField] string predicate;
+        [SerializeField] string[] parameters;
+
+        public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+        {
+            foreach (IPredicateEvaluator evaluator in evaluators)
+            {
+                bool? result = evaluator.Evaluate(predicate, parameters);
+                if (result == null) continue;
+                if (result == false) return false;
+            }
+            return true; // default state is true
+        }
+    }
+}
